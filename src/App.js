2246dce97cf608605a936aee507747b9cad2b465
karlmiche <karlymism@gmail.com>
2020-08-28 14:25:41 -0700

    fixed up some speech bubbles

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ function App() {
                     <h1>About Origami Pet</h1>
                     <p>A “Tamagotchi”-style pet game built with HTML5, CSS, and vanilla Javascript. Origami Pet utilizes conditional logic taken from the JavaScript Date Object - “needs” decrement based on seconds passed since a locally-stored timestamp, and increment (seconds are “added” to the locally stored timestamp) based on user click events. Each Pet created can be locally stored in a user’s browser - the result is a calm, sweet, and unique game-play.</p>
                   </section>
-                  <section className="step-4 step">
+                  <section className="step-4 step speech-bubble">
                     <p>Don't forget to feed your pet! Time passes quickly.</p>
                   </section>
               </div>
@@ -180,7 +180,7 @@ function App() {
                     <h1>About glisten</h1>
                     <p>An app built with those who process orally in mind, glisten is an audio transcription app built with Node.js, Google Cloud Speech to Text, Google Cloud Storage, Sequelize, Express, EJS layouts, and the Web Speech API.</p>
                   </section>
-                  <section className="step-8 step">
+                  <section className="step-8 step speech-bubble">
                   <p>My intended user is a busy academic who records many audio interviews while capturing data for social research. glisten is also good for people to play around with, to take voice notes with, and to transcribe long-winded clips of Gilmore Girls with (you don't want to miss all those jokes).</p>
                   </section>
               </div>
@@ -203,7 +203,7 @@ function App() {
                     <h1>About innervue</h1>
                     <p>innervue is a powerful sentiment analysis app built over 60+ hours of pair programming with two other developers. The goal is for job-seekers to receive higher positivity scores over time. The concept for this app came from developer Neri Ramirez. This app was built with MongoDB, Express, React.js, Node.js, the Google Cloud Natural Language API, the IBM Watson Sentiment Analysis API, and the Web Speech API.</p>
                   </section>
-                  <section className="step-10 step">
+                  <section className="step-10 step speech-bubble">
                   <p>Neri came up with this idea after our bootcamp's career coach mentioned that interviewees with positively framed interview answers seem more prepared. So far, I've learned a lot by using our app!</p>
                   </section>
               </div>
@@ -213,7 +213,7 @@ function App() {
                   <div className="bio-content">
                   <div className="bird-container">
                   <img className="center-bird" src="./Origami Pet.png"></img>
-                  <div className="speech-bubble">
+                  <div className="speech-bubble-three">
                     <h1>Thanks for stopping by!</h1>
                     </div>
                   </div>
